4b1b4eb617f645eb44d25b4a7be69c74d75d4490
nor1318 <iambobbydahal@gmail.com>
2025-12-08 08:59:32 +0545

    Fixed minor error in User Product Sho Page logic

--- app/Http/Controllers/UserProductController.php
+++ app/Http/Controllers/UserProductController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
 
 class UserProductController extends Controller
 {
